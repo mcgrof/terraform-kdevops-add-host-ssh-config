2a9311bc84908fee002a0bb5195c198e61a0d09f
Luis Chamberlain <mcgrof@kernel.org>
2019-08-15 18:18:55 -0700

    Initial import
    This is an initial import of the terraform module to add your hosts
    from terraform to your ssh configuration file.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

---
+++ README.md
@@ -0,0 +1,42 @@
+# terraform-kdevops-add-host-ssh-config
+
+This module lets you populate your ssh configuration file, typically
+` ~/.ssh/config` automatically from the created hosts.
+
+## Usage
+
+Below is an example usage:
+
+```
+locals {
+  limit_count = 4
+  shorthosts = openstack_compute_instance_v2.fstests_instances.*.name
+  ipv4s = openstack_compute_instance_v2.fstests_instances.*.access_ip_v4
+}
+
+module "ssh_config_update_host_entries" {
+  source = "./modules/update_ssh_config/"
+
+  ssh_config = var.ssh_config
+  update_ssh_config_enable = local.limit_count > 0 ? "true" : ""
+  cmd = "update"
+  shorthosts = join(",", slice(local.shorthosts, 0, local.limit_count))
+  hostnames = join(",", slice(local.ipv4s, 0, local.limit_count))
+  ports = "22"
+  user = var.ssh_config_user == "" ? "" : var.ssh_config_user
+  id = replace(var.ssh_pubkey_file, ".pub", "")
+  strict = var.ssh_config_use_strict_settings != "true" ? "" : "true"
+  use_backup = var.ssh_config_backup != "true" || var.ssh_config == "/dev/null" ? "" : "true"
+  backup_postfix = "kdevops"
+}
+```
+
+The ports can also be a comma separted list.
+
+### Author
+
+[Luis Chamberlain](https://www.do-not-panic.com)
+
+### License
+
+This module is released under the GPLv2.
---
+++ main.tf
@@ -0,0 +1,51 @@
+# update your ~/.ssh/config based on the provisioned host
+
+locals {
+  cmd_prefix_remove = var.update_ssh_config_enable != "" ? (var.cmd == "addhost" ? "echo Ignoring remove command" : "") : "echo Ignoring remove cmd: "
+  cmd_prefix_add = var.update_ssh_config_enable != "" ? (var.cmd == "remove" ? "echo Ignoring add command" : "") : "echo Ignoring add cmd: "
+  hostnames = var.hostnames == "" ? "" : format("--hostname %s", var.hostnames)
+  ports = var.ports == "" ? "" : format("--port %s", var.ports)
+  user = var.user == "" ? "" : format("--username %s", var.user)
+  id = var.id == "" ? "" : format("--identity %s", var.id)
+  strict = var.strict == "" ? "" : "--addstrict"
+  backup = var.use_backup == "" ? "" : format("--backup_file %s.backup.%s.",
+           var.ssh_config,
+           var.backup_postfix)
+}
+
+resource "null_resource" "ssh_config_remove" {
+  provisioner "local-exec" {
+    command = format("%s./update_ssh_config.py --remove %s %s %s %s %s %s %s %s",
+      local.cmd_prefix_remove,
+      var.shorthosts,
+      local.hostnames,
+      local.user,
+      local.ports,
+      local.id,
+      local.strict,
+      format("%s.%s", local.backup, "remove"),
+      var.ssh_config
+    )
+    working_dir = "modules/update_ssh_config/"
+  }
+}
+
+resource "null_resource" "ssh_config_add" {
+  provisioner "local-exec" {
+    command = format("%s./update_ssh_config.py --addhost %s %s %s %s %s %s %s %s",
+      local.cmd_prefix_add,
+      var.shorthosts,
+      local.hostnames,
+      local.user,
+      local.ports,
+      local.id,
+      local.strict,
+      format("%s.%s", local.backup, "add"),
+      var.ssh_config
+    )
+    working_dir = "modules/update_ssh_config/"
+  }
+  depends_on = [
+    "null_resource.ssh_config_remove",
+  ]
+}
