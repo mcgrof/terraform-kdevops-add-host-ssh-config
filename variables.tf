871cd662808425c77d7587f5245a711477037947
Luis Chamberlain <mcgrof@kernel.org>
2020-01-10 10:49:08 -0600

    variables.tf: remove quoted types
    terraform v0.12.19 complains about a quoted type constraints.
    
    Warning: Quoted type constraints are deprecated
    
      on .terraform/modules/ssh_config_update_host_entries/mcgrof-terraform-kdevops-add-host-ssh-config-ee46889/variables.tf line 2, in variable "ssh_config":
       2:   type = "string"
    
    Terraform 0.11 and earlier required type constraints to be given in quotes,
    but that form is now deprecated and will be removed in a future version of
    Terraform. To silence this warning, remove the quotes around "string".
    
    (and 10 more similar warnings elsewhere)
    
    This should fix that.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- variables.tf
+++ variables.tf
@@ -1,65 +1,65 @@
 variable "ssh_config" {
-  type = "string"
+  type = string
   default = "~/.ssh/config"
   description = "Your ssh configuration to edit"
 }
 
 variable "update_ssh_config_enable" {
-  type = "string"
+  type = string
   default = ""
   description = "Will enable this module if set to something other than empty"
 }
 
 variable "cmd" {
-  type = "string"
+  type = string
   default = "addhost"
   description = "Which command to use, supported: addhost, remove"
 }
 
 variable "shorthosts" {
-  type = "string"
+  type = string
   default = "kdevops-ignore-demo-host"
   description = "Comma separated names identifying the target host to work on"
 }
 
 variable "hostnames" {
-  type = "string"
+  type = string
   default = ""
   description = "Comma separated hostnames or IP addresses representing the shorthosts"
 }
 
 variable "ports" {
-  type = "string"
+  type = string
   default = ""
   description = "Comma separated ports or to use for each shorthost"
 }
 
 variable "user" {
-  type = "string"
+  type = string
   default = ""
   description = "Username to use"
 }
 
 variable "id" {
-  type = "string"
+  type = string
   default = ""
   description = "ssh identity file to use for this host, the ssh pubkey name"
 }
 
 variable "strict" {
-  type = "string"
+  type = string
   default = ""
   description = "If set to true we'll use strict host settings"
 }
 
 variable "use_backup" {
-  type = "string"
+  type = string
   default = ""
   description = "If set to true we'll use backup your ssh config"
 }
 
 variable "backup_postfix" {
-  type = "string"
+  type = string
   default = "kdevops_module"
   description = "The postfix to use for the backup file for your ssh config"
 }
